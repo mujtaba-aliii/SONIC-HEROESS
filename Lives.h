200a02cb8663004d334a205da29c52db381c3210
Your Name <i240726@isb.nu.edu.pk>
2025-05-11 15:56:37 +0500

    changes

--- Lives.h
+++ Lives.h
@@ -1,2 +1,31 @@
 #pragma once
+#include<iostream>
 #include <SFML/Graphics.hpp>
+#include "PlayerCharacter.h"
+class Lives {
+public:
+	Lives(float startX, float startY)
+		: x(startX), y(startY)
+	{
+		if (!font.loadFromFile("Data/arial.ttf")) {
+			std::cerr << "Failed to load font\n";
+		}
+		text.setFont(font);
+		text.setCharacterSize(24);
+		text.setFillColor(sf::Color::White);
+		text.setPosition(x, y);
+	}
+	void setLives(int lives) {
+		this->lives = lives;
+		text.setString("Lives: " + std::to_string(lives));
+	}
+	void render(sf::RenderWindow& window) {
+		window.draw(text);
+	}
+protected:
+	float x, y;
+	int lives = 3; // Default number of lives
+	sf::Font font;
+	sf::Text text;
+	sf::Texture texture;
+};
